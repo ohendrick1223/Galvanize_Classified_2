df9622e03fb78f7eb92b7d60eccbc15c942693ce
Olivia <ohendrick1223@gmail.com>
2017-01-30 15:59:08 -0700

    get edit functionality working on post objects

--- public/js/classifieds.component.js
+++ public/js/classifieds.component.js
@@ -19,6 +19,20 @@
           vm.allPosts = response.data;
         });
       };
+
+      vm.deletePost = function(id) {
+        console.log("I am getting to Delete");
+        var postId = id;
+        console.log(postId);
+
+        $http.delete(`classifieds/${postId}`).then(function(response) {
+          console.log("deleted: ", response.data);
+          $http.get('/classifieds').then(function(response) {
+            console.log("array of classifieds", response.data);
+            vm.allPosts = response.data;
+          });
+        });
+      };
     }
         //make http calls for posting, editing, get by id, deleting
 
