f3223925b71b25f8420de25f850f8dafd9239902
Olivia <ohendrick1223@gmail.com>
2017-01-31 17:46:50 -0700

    got posting working again!

--- public/js/post.service.js
+++ public/js/post.service.js
@@ -1,11 +1,11 @@
 (function() {
   'use strict';
-
   angular.module('app')
   .service('postService', function($http) {
     this.allPosts = [];
     this.submitPost = function(post) {
-      return $http.post('/classifieds', post).then(function(response) {
+      console.log("getting inside service");
+       $http.post('/classifieds', post).then(function(response) {
         console.log("getting inside post request", response.data);
         this.allPosts = response.data;
         console.log(response.data);
